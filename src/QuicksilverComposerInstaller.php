5605103ed1238d6859f183b3aa5b59edb01349fa
rvtraveller <rvtraveller@checkthelog.com>
2016-12-11 21:48:57 -0500

    Add a default install path.

--- src/QuicksilverComposerInstaller.php
+++ src/QuicksilverComposerInstaller.php
@@ -11,6 +11,13 @@ class QuicksilverComposerInstaller extends LibraryInstaller
   /**
    * {@inheritDoc}
    */
+  public function getInstallPath(PackageInterface $package) {
+    return 'private/quicksilver/' . $package->getPrettyName();
+  }
+
+  /**
+   * {@inheritDoc}
+   */
   public function supports($packageType)
   {
   	var_dump("PACKAGE TYPE: " . $packageType);
