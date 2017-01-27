500f9bc2161d0d96af24c688ad313ea7617adade
Greg Anderson <greg.1.anderson@greenknowe.org>
2017-01-27 11:57:35 -0800

    Try to work around strange autoload problem on CircleCI.

--- src/QuicksilverComposerInstallerPlugin.php
+++ src/QuicksilverComposerInstallerPlugin.php
@@ -10,6 +10,10 @@ class QuicksilverComposerInstallerPlugin implements PluginInterface
 {
   public function activate(Composer $composer, IOInterface $io)
   {
+    // Strange autoloading problem on CircleCI
+    if (!class_exists(QuickSilverComposerInstaller::class)) {
+      include_once __DIR__ . '/QuicksilverComposerInstaller.php';
+    }
     $installer = new QuickSilverComposerInstaller($io, $composer);
     $composer->getInstallationManager()->addInstaller($installer);
   }
